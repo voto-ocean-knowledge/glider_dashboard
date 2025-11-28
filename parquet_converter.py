09bf1f3af0b9607e4f85aabbf86af49d9f8b835c
MartinMohrmann <m.j.mohrmann@gmail.com>
2025-11-28 16:03:53 +0100

    forgot converter

--- parquet_converter.py
+++ parquet_converter.py
@@ -1,4 +1,5 @@
 import glob
+from pathlib import Path
 
 import numpy as np
 import polars as pl
@@ -7,6 +8,10 @@ import xarray as xr
 nc_datasets = glob.glob("../voto_erddap_data_cache/*.nc")
 print("now converting all files to parquet, please wait...")
 for file in nc_datasets:
-    df = xr.open_dataset(file, drop_variables="ad2cp_time").to_pandas()
-    df = pl.from_dataframe(df.astype(np.float32))
-    df.write_parquet(file.replace("nc", "parquet"))
+    if Path(file.replace("nc", "parquet")).is_file():
+        print(f"{file.replace('nc', 'parquet')} already exists, skip")
+        continue
+    else:
+        df = xr.open_dataset(file, drop_variables="ad2cp_time").to_pandas()
+        df = pl.from_dataframe(df.astype(np.float32))
+        df.write_parquet(file.replace("nc", "parquet"))
