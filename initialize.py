ef341e199fe5a0ba4acc1bdae73097ad88262511
MartinMohrmann <m.j.mohrmann@gmail.com>
2026-05-06 17:34:38 +0200

    GDAC download

--- initialize.py
+++ initialize.py
@@ -1,4 +1,5 @@
 import os.path
+import time
 import urllib.request
 from urllib.request import urlretrieve
 
@@ -200,3 +201,6 @@ if utils.GDAC_data:
             os.path.join(utils.cache_location, f"{dsid}_*.nc"), preprocess=preprocess
         )
         ds.to_netcdf(filepath)
+        time.wait(
+            60
+        )  # I assume the low-spec server needs a bit of time to recover from download
