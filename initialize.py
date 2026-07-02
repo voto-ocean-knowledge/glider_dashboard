d23d7d41d011a0ea992f9077a789c782a6bfdf75
MartinMohrmann <m.j.mohrmann@gmail.com>
2026-07-02 14:55:56 +0200

    only redownload nrts

--- initialize.py
+++ initialize.py
@@ -76,10 +76,11 @@ for dataset_id in all_dataset_ids:
     e.dataset_id = dataset_id
     url = e.get_download_url()
     filepath = os.path.join(utils.cache_location, f"{dataset_id}.nc")
-    less_than_60d_old = allDatasetsVOTO["minTime (UTC)"].dt.tz_convert(None).loc[
-        dataset_id
-    ] > (datetime.datetime.now() - datetime.timedelta(days=60))
-    if os.path.isfile(filepath) and not less_than_60d_old:
+    isrecent_nrt = (
+        allDatasetsVOTO["minTime (UTC)"].dt.tz_convert(None).loc[dataset_id]
+        > (datetime.datetime.now() - datetime.timedelta(days=60))
+    ) and (dataset_id[0:3] == "nrt")
+    if os.path.isfile(filepath) and not isrecent_nrt:
         print("file already exists, skip and continue")
         continue
     urlretrieve(url, filepath)
